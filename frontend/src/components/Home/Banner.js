d334c670b19d74e428a0e36bdb990179f703c6d9
vineetpradhan <vineetpradhan1@gmail.com>
2022-08-08 21:12:55 +0700

    set show input

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
+  const [showInput, setShowInput] = useState(0);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            id="search-box"
-            placeholder="What is it that you truly desire?"
-            value={props.term}
-            onChange={(e) => props.setTerm(e.target.value)}
-          />
+          <span id="get-part" onClick={() => setShowInput(1)}>
+            A place to get
+          </span>
+          {showInput ? (
+            <input
+              id="search-box"
+              placeholder="What is it that you truly desire?"
+              value={props.term}
+              onChange={(e) => props.setTerm(e.target.value)}
+            />
+          ) : null}
           <span> the cool stuff.</span>
         </div>
       </div>
