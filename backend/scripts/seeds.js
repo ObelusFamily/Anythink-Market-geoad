f653d1b3a2e5a0c0a9dcdc8c61b39e451df84c1d
v1neetpradhn <110778122+v1neetpradhn@users.noreply.github.com>
2022-08-10 19:08:50 +0700

    Update seeds.js

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -9,23 +9,44 @@ const Item = require("../models/Item");
 const Comment = require("../models/Comment");
 
 const seed = async (i) => {
-  let u = new User({
-    email: `${i}@email.com`,
-    username: i,
-  });
-  u.setPassword(`${i}`);
-  const user = await u.save();
-  var i = new Item({ title: i, description: "", tagList: [] });
-  i.seller = user;
-  const item = await i.save();
-  var c = new Comment({ body: "nice" });
-  c.item = item;
-  c.seller = user;
-  const comment = await c.save();
   i.comments = item.comments.concat([comment]);
   await i.save();
 };
+let users = [];
+let items = [];
+let comments = [];
+const seedUsers = async () => {
+  for (let i = 0; i < 100; i++) {
+    let u = new User({
+      email: `${i}@email.com`,
+      username: i,
+    });
+    u.setPassword(`${i}`);
+    users.push(u);
+  }
+  await User.insertMany(users);
+};
+const seedItems = async () => {
+  for(let i = 0; i < 100; i++) {
+    var x = new Item({ title: i, description: "", tagList: [] });
+    x.seller = users[i];
+    items.push(x);
+  }
+  await Item.insertMany(items);
+};
+const seedComments = async () => {
+  for(let i = 0; i < 100; i++) {
+    var c = new Comment({ body: "nice" });
+    c.item = items[0];
+    c.seller = users[i];
+    comments.push(c);
+  }
+  await Comment.insertMany(comments);
+  i.comments = item.comments.concat([comment]);
+  let i = items[0];
+  i.comments = items[0].comments
+};
 
-for (let i = 0; i < 100; i++) {
-  seed(i);
-}
+seedUsers();
+seedItems();
+seedComments();
